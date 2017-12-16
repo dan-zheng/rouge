9eb2e73a71da7edf28f7c98070520fb8d7966565
Dan Zheng <zheng321@purdue.edu>
2017-12-16 09:57:23 -0500

    [DLVM] Start lexer

---
+++ lib/rouge/lexers/dlvm.rb
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*- #
+
+module Rouge
+  module Lexers
+    class DLVM < RegexLexer
+      title "DLVM"
+      desc 'The DLVM Compiler Infrastructure (http://dlvm.org/)'
+      tag 'dlvm'
+
+      filenames '*.dl'
+      mimetypes 'text/x-dlvm'
+
+      string = /"[^"]*?"/
+      identifier = /([-a-zA-Z$._][-a-zA-Z$._0-9]*|#{string})/
+      use = /(%[0-9][1-9]*.[0-9][1-9]*)/
+      id = /(%#{identifier}|#{use})/
+
+      state :basic do
+        rule %r(\/\/.*?$), Comment::Single
+        rule /\s+/, Text
+
+        rule /'#{identifier}\s*(?=[(])/, Name::Label
+
+        rule /@#{identifier}/, Name::Function
+        rule /#{id}\s*/, Name::Variable
+
+        rule /c?#{string}/, Str
+
+        rule /0[xX][a-fA-F0-9]+/, Num
+        rule /-?\d+(?:[.]\d+)?(?:[eE][-+]?\d+(?:[.]\d+)?)?/, Num
+
+        rule /[=<>{}\[\]()*:.,]|x/, Punctuation
+        rule %r([-/=+*%<>!&|^.~•⨂]+), Operator
+      end
+
+      builtin_types = %w(
+        f16 f32 f64 bool
+      )
+
+      state :types do
+        rule /i[1-9]\d*/, Keyword::Type
+        rule /#{builtin_types.join('|')}/, Keyword::Type
+      end
+
+      builtin_keywords = %w(
+        module stage raw optimizable compute scheduled canonical struct func var
+        type opaque at to from by upto then else wrt keeping void zero undefined
+        null true false scalar count seedable extern gradient init along
+      )
+
+      builtin_instructions = %w(
+        literal branch conditional return dataTypeCast scan reduce dot matrixMultiply
+        concatenate transpose slice shapeCast bitCast extract insert apply allocateStack
+        allocateHeap allocateBox projectBox retain release deallocate load store
+        elementPointer copy trap random select lessThan lessThanOrEqual greaterThan
+        greaterThanOrEqual equal notEqual and or add subtract multiply divide min max
+        truncateDivide floorDivide modulo power mean sinh cosh tanh log exp negate
+        sign square sqrt round rsqrt ceil floor tan cos sin acos asin atan lgamma
+        digamma erf erfc rint not x f16 f32 f64 bool
+      )
+
+      state :keywords do
+        rule /#{builtin_instructions.join('|')}/, Keyword
+        rule /#{builtin_keywords.join('|')}/, Keyword
+      end
+
+      state :root do
+        mixin :basic
+        mixin :keywords
+        mixin :types
+      end
+    end
+  end
+end
